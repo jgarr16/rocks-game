71b210619d6468490a2e95beeff07fe81ccd2861
jrgarrigues <jgarr16@users.noreply.github.com>
2021-05-16 14:18:45 -0700

    added more verifications for player names, and redid the way the rolls get verified

--- rpsgame.py
+++ rpsgame.py
@@ -3,36 +3,63 @@ print('------------------------')
 print('  Rock Paper Scissors v1')
 print('------------------------')
 
-player_1 = "John" # input("Enter player 1's name: ")
-player_2 = "Kyong" # input("Enter player 2's name: ")
+player_1 = input("Enter player 1's name: ")
+player_1 = player_1.title().strip()
+player_2 = input("Enter player 2's name: ")
+player_2 = player_2.title().strip()
 
 rolls = ["rock","paper","scissors"]
 
-roll1 = input(f'{player_1}, what is your roll? [rock, paper, scissors]: ').strip()
+roll1 = input(f'{player_1}, what is your roll? [rock, paper, scissors]: ')
+roll1 = roll1.lower().strip()
 if roll1 not in rolls:
     print(f"Sorry {player_1}, {roll1} is not a valid choice.")
-roll2 = input(f'{player_2}, what is your roll? [rock, paper, scissors]: ').strip()
+roll2 = input(f'{player_2}, what is your roll? [rock, paper, scissors]: ')
+roll2 = roll2.lower().strip()
 if roll2 not in rolls:
-    print(f"Sorry {player_2}, {roll2} is not a valid choice.")
+    print(f"Sorry {player_2}, {roll2} is not a valid play.")
 
-if roll1.lower() == rolls[0]:
-    if roll2.lower() == rolls[0]:
-        print(f"It's a tie, try again.")
-    elif roll2.lower() == rolls[1]:
-        print(f"{player_2}'s {roll2.lower()} beats {player_1}'s {roll1.lower()}.")
+
+# Test for a winner
+winner = None
+
+if roll1 == roll2:
+    winner = None
+elif roll1 == rolls[0]:
+    if roll2 == rolls[1]:
+        winner = player_2
+        winplay = roll2
+        looser = player_1
+        losplay = roll1
     else:
-        print(f"{player_1}'s {roll1.lower()} beats {player_2}'s {roll2.lower()}.")
-if roll1.lower() == rolls[1]:
-    if roll2.lower() == rolls[1]:
-        print(f"It's a tie, try again.")
-    elif roll2.lower() == rolls[2]:
-        print(f"{player_2}'s {roll2.lower()} beats {player_1}'s {roll1.lower()}.")
+        winner = player_1
+        winplay = roll1
+        looser = player_2
+        losplay = roll2
+elif roll1 == rolls[1]:
+    if roll2 == rolls[2]:
+        winner = player_2
+        winplay = roll2
+        looser = player_1
+        losplay = roll1
     else:
-        print(f"{player_1}'s {roll1.lower()} beats {player_2}'s {roll2.lower()}.")
-if roll1.lower() == rolls[2]:
-    if roll2.lower() == rolls[2]:
-        print(f"It's a tie, try again.")
-    elif roll2.lower() == rolls[0]:
-        print(f"{player_2}'s {roll2.lower()} beats {player_1}'s {roll1.lower()}.")
+        winner = player_1
+        winplay = roll1
+        looser = player_2
+        losplay = roll2
+elif roll1 == rolls[2]:
+    if roll2 == rolls[0]:
+        winner = player_2
+        winplay = roll2
+        looser = player_1
+        losplay = roll1
     else:
-        print(f"{player_1}'s {roll1.lower()} beats {player_2}'s {roll2.lower()}.")
+        winner = player_1
+        winplay = roll1
+        looser = player_2
+        losplay = roll2
+
+if winner is None:
+    print(f"Nobody won, the play was a tie.")
+else:
+    print(f"{winplay.title()} beats {losplay}; {winner} wins the play!")
