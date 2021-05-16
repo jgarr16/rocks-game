a26859f4ccc8bd43f834866fb536f4369a3d4160
jrgarrigues <jgarr16@users.noreply.github.com>
2021-05-16 07:19:26 -0700

    Initial commit

---
+++ rpsgame.py
@@ -0,0 +1,13 @@
+
+print('------------------------')
+print('  Rock Paper Scissors v1')
+print('------------------------')
+
+player_1 = input("Enter player 1's name: ")
+player_2 = input("Enter player 2's name: ")
+
+rolls = ["rock","paper","scissors"]
+
+print(rolls)
+print(player_1)
+print(player_2)
