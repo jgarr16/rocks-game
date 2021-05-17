00e1618e8ab722c980888ad4c7a35b1ce80378c8
jrgarrigues <jgarr16@users.noreply.github.com>
2021-05-16 18:43:24 -0700

    built out functions for all the big items

--- rpsgame.py
+++ rpsgame.py
@@ -1,67 +1,95 @@
 import random
 
-
-print('------------------------')
-print('  Rock Paper Scissors v1')
-print('------------------------')
-
-player_1 = "You" # input("Enter player 1's name: ")
-player_1 = player_1.title().strip()
-player_2 = "Borg" # input("Enter player 2's name: ")
-player_2 = player_2.title().strip()
-
-rolls = ["rock","paper","scissors"]
-
-roll1 = input(f'{player_1}, what is your roll? [rock, paper, scissors]: ')
-roll1 = roll1.lower().strip()
-if roll1 not in rolls:
-    print(f"Sorry {player_1}, {roll1} is not a valid choice.")
-roll2 = random.choice(rolls) # input(f'{player_2}, what is your roll? [rock, paper, scissors]: ')
-roll2 = roll2.lower().strip()
-if roll2 not in rolls:
-    print(f"Sorry {player_2}, {roll2} is not a valid play.")
-
-
-# Test for a winner
 winner = None
 
-if roll1 == roll2:
-    winner = None
-elif roll1 == rolls[0]:
-    if roll2 == rolls[1]:
-        winner = player_2
-        winplay = roll2
-        looser = player_1
-        losplay = roll1
-    else:
-        winner = player_1
-        winplay = roll1
-        looser = player_2
-        losplay = roll2
-elif roll1 == rolls[1]:
-    if roll2 == rolls[2]:
-        winner = player_2
-        winplay = roll2
-        looser = player_1
-        losplay = roll1
-    else:
-        winner = player_1
-        winplay = roll1
-        looser = player_2
-        losplay = roll2
-elif roll1 == rolls[2]:
-    if roll2 == rolls[0]:
-        winner = player_2
-        winplay = roll2
-        looser = player_1
-        losplay = roll1
-    else:
-        winner = player_1
-        winplay = roll1
-        looser = player_2
-        losplay = roll2
 
-if winner is None:
-    print(f"Nobody won, the play was a tie.")
-else:
-    print(f"{winplay.title()} beats {losplay}; {winner} wins the play!")
+
+def main():
+    """docstring"""
+    show_header()
+    player = "John"
+    ai = "Borg"
+    play_game(player, ai)
+
+
+def show_header():
+    """docstring"""
+    print('------------------------')
+    print('  Rock Paper Scissors v1')
+    print('------------------------')
+
+
+def play_game(player_1, player_2):
+    """docstring"""
+    rolls = ["rock", "paper", "scissors"]
+
+    roll1 = get_roll(player_1, rolls)
+    roll2 = random.choice(rolls)
+
+    losplay, winner, winplay = check_for_winning_throw(player_1, player_2, roll1, roll2, rolls)
+
+    if winner is None:
+        print(f"Nobody won, the play was a tie.")
+    elif winner == "foul1":
+        print(f"{player_1}'s roll was invalid.")
+    elif winner == "foul2":
+        print(f"{player_2}'s roll was invalid.")
+    else:
+        print(f"{winplay.title()} beats {losplay}; {winner} wins the play!")
+
+
+def check_for_winning_throw(player_1, player_2, roll1, roll2, rolls):
+    # Test for a winner
+    losplay = None
+    winplay = None
+    if roll1 == roll2:
+        winner = None
+    elif roll1 == "foul":
+        winner = "foul1"
+    elif roll1 == rolls[0]:
+        if roll2 == rolls[1]:
+            winner = player_2
+            winplay = roll2
+            looser = player_1
+            losplay = roll1
+        else:
+            winner = player_1
+            winplay = roll1
+            looser = player_2
+            losplay = roll2
+    elif roll1 == rolls[1]:
+        if roll2 == rolls[2]:
+            winner = player_2
+            winplay = roll2
+            looser = player_1
+            losplay = roll1
+        else:
+            winner = player_1
+            winplay = roll1
+            looser = player_2
+            losplay = roll2
+    elif roll1 == rolls[2]:
+        if roll2 == rolls[0]:
+            winner = player_2
+            winplay = roll2
+            looser = player_1
+            losplay = roll1
+        else:
+            winner = player_1
+            winplay = roll1
+            looser = player_2
+            losplay = roll2
+    return losplay, winner, winplay
+
+
+def get_roll(player_name, rolls):
+    """`get-roll` gets the call from Player1 for the current play and validates it."""
+    roll = input(f'{player_name}, what is your roll? [rock, paper, scissors]: ')
+    roll = roll.lower().strip()
+    if roll not in rolls:
+        roll = "foul"
+    return roll
+
+
+if __name__ == '__main__':
+    main()
